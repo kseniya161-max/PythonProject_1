1b00e7f910ab2bb5ccdca4983ed2bd4254bacbac
Kseniya <baharevaxen@yandex.ru>
2025-07-10 20:19:39 +0300

    editing the length

--- src/feature/processing.py
+++ src/feature/processing.py
@@ -1,8 +1,15 @@
-operation = [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}, {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'}, {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]
+operation = [{'id': 41428829, 'state': 'EXECUTED',
+              'date': '2019-07-03T18:35:29.512364'},
+             {'id': 939719570, 'state': 'EXECUTED',
+              'date': '2018-06-30T02:08:58.425572'},
+             {'id': 594226727, 'state': 'CANCELED',
+              'date': '2018-09-12T21:27:25.241689'},
+             {'id': 615064591, 'state': 'CANCELED',
+              'date': '2018-10-14T08:21:33.419441'}]
 
 
-def filter_by_state(operations: list, state: str = 'EXECUTED') -> list:
-    """Функция принимает список словарей и возвращает новый список словарей с ключем state по значению"""
+def filter_by_state(operation: list, state: str = 'EXECUTED') -> list:
+    """Сортирует список словарей по значению state"""
     result = []
     for operat in operation:
         if operat.get('state') == state:
@@ -14,9 +21,8 @@ print(filter_by_state(operation, 'EXECUTED'))
 print(filter_by_state(operation, 'CANCELED'))
 
 
-
 def sort_by_date(operation: list) -> list:
-    """Функция принимает список словарей и возвращает список отсортированный по дате"""
+    """Сортирует список словарей по дате"""
     return sorted(operation, key=lambda x: x["date"], reverse=True)
 
 
